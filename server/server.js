c74db489806042c65b6d2a90f83fc9cf53eeca42
dfive <govindlucky29@gmail.com>
2017-06-03 21:46:18 +0530

    otp code

--- server/server.js
+++ server/server.js
@@ -10,14 +10,14 @@ var app=express();
 
 app.use(bodyParser.json());
 
-app.post('/sendotp',(req,res)=>{
+app.post('/sendotp/',(req,res)=>{
   var {phonenumber} = _.pick(req.body, ['phonenumber']);
   request.post('https://2factor.in/API/V1/'+api_key+`/SMS/+${phonenumber}/AUTOGEN`,(err,response,body)=>{
     res.status(200).send(body);
   });
 })
 
-app.post('/verifyotp',(req,res)=>{
+app.post('/verifyotp/',(req,res)=>{
   var input=_.pick(req.body,['sessionid','otp']);
   request.post(`https://2factor.in/API/V1/${api_key}/SMS/VERIFY/${input.sessionid}/${input.otp}`,(err,response,body)=>{
     res.status(200).send(body);
